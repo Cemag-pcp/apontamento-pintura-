d2bdc099fc39257029f1347fab27c89415feada6
TI <pcp@cemag.com.br>
2023-06-05 15:09:42 -0300

    atualizando dados da tabela

--- app.py
+++ app.py
@@ -4,11 +4,12 @@ from oauth2client.service_account import ServiceAccountCredentials
 import pandas as pd
 import time
 from gspread_formatting import CellFormat, format_cell_range
+import datetime
 
 app = Flask(__name__)
 app.secret_key = "apontamentopintura"
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def gerar_cambao():
 
     def get_sheet_data_gerar():
@@ -38,35 +39,70 @@ def gerar_cambao():
 
         table['TIPO'] = ''
 
-        table = table[['CODIGO', 'DESCRICAO', 'QT_ITENS', 'COR', 'PROD.']]
-
+        table = table[['DATA DA CARGA','CODIGO', 'DESCRICAO', 'QT_ITENS', 'COR', 'PROD.']]
+        
         values = table.values.tolist()
 
         return values , table
     
+    if request.method == 'POST':
+        filtro_data = request.form.get('filtro_data')
+        filtro_cor = request.form.get('filtro_cor')
+
+        hoje = datetime.datetime.today().strftime("%d/%m/%Y")
+
+        sheet_data, table = get_sheet_data_gerar()
+
+        if filtro_data == '':
+            table = table[table['DATA DA CARGA'] == hoje]
+            cores = table['COR'].drop_duplicates().values.tolist()
+
+        else:
+            filtro_data = datetime.datetime.strptime(filtro_data, "%Y-%m-%d").strftime("%d/%m/%Y")
+            table = table[table['DATA DA CARGA'] == filtro_data]
+        
+            cores = table['COR'].drop_duplicates().values.tolist()
+
+        if filtro_cor != 'Todas':
+            table = table[table['COR'] == filtro_cor]
+        else:
+            pass 
+
+        sheet_data = table.values.tolist()
+
+        return render_template('gerar_cambao.html', sheet_data=sheet_data,cores=cores)
+
     sheet_data, table = get_sheet_data_gerar()
 
-    print(table)
+    hoje = datetime.datetime.today().strftime("%d/%m/%Y")
 
-    return render_template('gerar_cambao.html', sheet_data=sheet_data)
+    table = table[table['DATA DA CARGA'] == hoje]
+    cores = table['COR'].drop_duplicates().values.tolist()
 
-@app.route('/finalizarcambao')
-def finalizar_cambao():
+    sheet_data = table.values.tolist()
+
+    return render_template('gerar_cambao.html', sheet_data=sheet_data,cores=cores)
+
+@app.route('/send_gerar', methods=['GET','POST'])
+def gerar_planilha():
+
+    dados = request.get_json()
     
-    scope = ['https://www.googleapis.com/auth/spreadsheets',
-             "https://www.googleapis.com/auth/drive"]
-    
-    credentials = ServiceAccountCredentials.from_json_keyfile_name("service_account.json", scope)
-    client = gspread.authorize(credentials)
-    sa = gspread.service_account('service_account.json')    
+    table_final = pd.DataFrame(dados)
 
-    name_sheet1 = 'Base ordens de produçao finalizada'
-    worksheet1 = 'Pintura'
-    sh1 = sa.open(name_sheet1)
-    wks1 = sh1.worksheet(worksheet1)
-    list2 = wks1.get_all_records()
-    table1 = pd.DataFrame(list2)
-    return render_template('finalizar_cambao.html')
+    table_final['UNICO'] = ''
+
+    table_final['SETOR'] = 'Pintura'
+
+    table_final = table_final[table_final['prod'] != '']
+
+    for i in range(len(table_final)):
+        if table_final['tipo'][i] == '':
+            table_final['tipo'][i] = 'PO'
+
+    print(table_final)
+
+    return 'Planilha gerada com sucesso!'
 
 if __name__ == '__main__':
     app.run(debug=True)
